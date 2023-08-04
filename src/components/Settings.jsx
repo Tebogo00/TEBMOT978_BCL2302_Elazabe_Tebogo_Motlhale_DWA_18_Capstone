e40e57611899503716f8ece80298f360060bcf8e
Tebogo00 <motlhalet420@gmail.com>
2023-08-04 16:46:05 +0200

    DWA_18

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,1 @@
 
-export const settings = {
-    dots: true,
-    infinite: true,
-    speed: 5500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplaySpeed : 10000,
-    autoplay : true
-}
